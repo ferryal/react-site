9421a5a41a0ec09cbe1fc3f22b95dc92e5f9f723
ferryal <ferryal.bahari@amartha.com>
2021-01-23 05:25:58 +0700

    add constant text

---
+++ src/constants/index.js
@@ -0,0 +1,40 @@
+export const skills = [
+  {
+    name: "consult",
+    description:
+      "Our work then targeted, best practices outcomes social innovation synergy. Venture philanthropy, revolutionary inclusive policymaker relief. User-centered program areas scale.",
+    class: "fas fa-comments"
+  },
+  {
+    name: "Design",
+    description:
+      "Policymaker collaborates collective impact humanitarian shared value vocabulary inspire issue outcomes agile. Overcome injustice deep dive agile issue outcomes vibrant boots on the ground sustainable.",
+    class: "fas fa-paint-brush"
+  },
+  {
+    name: "Develop",
+    description:
+      "Revolutionary circular, movements a or impact framework social impact low-hanging. Save the compelling revolutionary inspire progress. Collective impacts and challenges for opportunities of thought provoking.",
+    class: "fas fa-cubes"
+  },
+  {
+    name: "Marketing",
+    description:
+      "Peaceful; vibrant paradigm, collaborative cities. Shared vocabulary agile, replicable, effective altruism youth. Mobilize commitment to overcome injustice resilient, uplift social transparent effective.",
+    class: "fas fa-bullhorn"
+  },
+  {
+    name: "Manage",
+    description:
+      "Change-makers innovation or shared unit of analysis. Overcome injustice outcomes strategize vibrant boots on the ground sustainable. Optimism, effective altruism invest optimism corporate social.",
+    class: "fas fa-bars"
+  },
+  {
+    name: "Evolve",
+    description:
+      "Activate catalyze and impact contextualize humanitarian. Unit of analysis overcome injustice storytelling altruism. Thought leadership mass incarceration. Outcomes big data, fairness, social game-changer.",
+    class: "fas fa-chart-line"
+  }
+];
+
+export const TEN_MINUTES_IN_MILISEC = 600000;
